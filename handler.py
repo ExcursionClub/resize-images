3dcf154c0a8cfc6830da3be104ada08e93797fcf
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-11-06 12:31:31 +0100

    Duplicate hello function

--- handler.py
+++ handler.py
@@ -1,9 +1,24 @@
-import json
-
-
 def hello(event, context):
-    return dict(
-        statusCode=200,
-        body="Hello"
-    )
+    try:
+        return {
+            'statusCode': 200,
+            'body': "Hello"
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': str(e)
+        }
 
+
+def resize_image(event, context):
+    try:
+        return {
+            'statusCode': 200,
+            'body': "Image re-sized!"
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': str(e)
+        }
