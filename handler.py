3d03d23080076dcd040f78bd47d1bd7e745b8139
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-11-07 19:48:11 +0100

    Remove hello endpoint

--- handler.py
+++ handler.py
@@ -1,19 +1,6 @@
 from resize_image import resize_image
 
 
-def handle_hello(event, context):
-    try:
-        return {
-            'statusCode': 200,
-            'body': "Hello"
-        }
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': str(e)
-        }
-
-
 def handle_resize_image(event, context):
     resize_image(event)
     print('handle_resize called')
